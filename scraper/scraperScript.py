11ddbd75487e77ce3c1c93dd54dce82e93f02cf5
JoKeR-VIKING <prathamvasani1@gmail.com>
2022-01-24 14:54:20 +0530

    ignore scraper

--- scraper/scraperScript.py
+++ scraper/scraperScript.py
@@ -1,4 +1,4 @@
-from urllib import request
+import urllib3
 from bs4 import BeautifulSoup
 import pymongo
 
@@ -11,10 +11,11 @@ def getPrice(productName):
 
     url = urlList[0]
     url = url.format(productName)
-    price = 0
+    price = "0"
 
-    response = request.urlopen(url)
-    soup = BeautifulSoup(response.read().decode('utf-8'), 'html.parser')
+    http = urllib3.PoolManager()
+    response = http.request('GET', url)
+    soup = BeautifulSoup(response.data, 'html.parser')
 
     search_result = soup.find_all('div', attrs = {'data-component-type': 's-search-result'})
 
@@ -26,8 +27,11 @@ def getPrice(productName):
         except:
             continue
 
-    productName.replace('+', ' ')
-    return [productName, price, url]
+    number_price = price
+    number_price = float(number_price.replace(',', ''))
+
+    productName = productName.replace('+', ' ')
+    return [productName, price, number_price, url]
 
 def getComponents():
     client = pymongo.MongoClient("mongodb://localhost:27017/")
@@ -36,20 +40,30 @@ def getComponents():
 
     allBuilds = col.find()
     for build in allBuilds:
+        total_price = 0
+
         for component in components:
             name = build[component][0]
             name = name.lower()
 
             if name[-1] != '*':
+                details = getPrice(name)
                 buildId = build['_id']
                 col.update_one({'_id': buildId}, {'$set': {
-                    component: getPrice(name)
+                    component: details
                 }})
             else:
+                details = [name, "0", 0, ""]
                 buildId = build['_id']
                 col.update_one({'_id': buildId}, {'$set': {
-                    component: [name, "0", ""]
+                    component: details
                 }})
+
+            total_price += details[2]
+
+            col.update_one(({'_id': buildId}), {'$set': {
+                'price': total_price
+            }})
 
 if __name__ == '__main__':
     getComponents()
