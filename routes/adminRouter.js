0aabc6a14bb6103b18b66d0a1c25e7438cbbc5f7
JoKeR-VIKING <prathamvasani1@gmail.com>
2022-01-31 16:23:14 +0530

    add form error try three

--- routes/adminRouter.js
+++ routes/adminRouter.js
@@ -12,6 +12,6 @@ router.get('/', function (req, res) {
     });
 });
 router.post('/add', adminController.addBuild);
-router.post('/compatible', adminController.addBuild);
+router.post('/compatible', adminController.compatible);
 
 module.exports = router;
